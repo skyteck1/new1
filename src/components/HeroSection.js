d37f95df16313e707562c4d07746951611484155
skyteck1 <dgnr3.ts3@gmail.com>
2023-01-12 14:23:16 +0530

    update images path

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -25,7 +25,7 @@ const HeroSection = ({ myData }) => {
           <div className="hero-section-image">
             <figure>
               <img
-                src="./images/hero.png"
+                src="./images/hero.jpg"
                 alt="hero-section-photo"
                 className="img-style"
               />
